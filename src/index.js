ac014b6f680ae49facf43e5927fafca784f71480
yppelu <yppelu@gmail.com>
2023-11-25 19:13:53 +0700

    Set up a structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './index.css';
+
+/* processWeatherData();
+
+async function getWeatherData(cityName) {
+  const forecastResponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4f889e1d89284a96bd475947232311&q=${cityName}&days=3`, { mode: 'cors' });
+  return await forecastResponse.json();
+}
+
+async function processWeatherData() {
+  const forecast = await getWeatherData('novosibirsk');
+  console.log(forecast);
+} */
